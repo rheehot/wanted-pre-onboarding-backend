57aef242c2b0df5808ac90c078db7dcaae616cc3
yunhyeji <dbsgpwl97@gmail.com>
2022-10-13 18:20:15 +0900

    feat: 채용공고 수정 API 구현

--- company/urls.py
+++ company/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from company.views import PostJobView
+from company.views import PostJobView, UpdateJobView
 
 urlpatterns = [
     path("/job/post", PostJobView.as_view()),
+    path("/job/<int:job_id>/update", UpdateJobView.as_view()),
 
 ]
--- company/views.py
+++ company/views.py
@@ -28,3 +28,25 @@ class PostJobView(View):
 
         except KeyError:
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+
+class UpdateJobView(View):
+    def post(self, request, job_id):
+        try:
+            job_data = json.loads(request.body)
+            position = job_data["position"]
+            incentive = job_data["incentive"]
+            detail = job_data["detail"]
+            stack = job_data["stack"]
+
+            Job.objects.filter(id=job_id).update(
+                position=position,
+                incentive=incentive,
+                detail=detail,
+                stack=stack
+            )
+
+            return JsonResponse({"MESSAGE": "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
